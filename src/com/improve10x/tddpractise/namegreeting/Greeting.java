bd7f9c2e363bc70cf8c9fcc0d52abfe43e2ebec0
roopa <roopa.peddinti@improve10x.com>
2023-07-18 15:09:08 +0530

    test cases passed for the name greeting problem

--- src/com/improve10x/tddpractise/namegreeting/Greeting.java
+++ src/com/improve10x/tddpractise/namegreeting/Greeting.java
@@ -2,6 +2,7 @@ package com.improve10x.tddpractise.namegreeting;
 
 public class Greeting {
     public String greetingName(String name) {
-        return name;
+        String greetingName = "Hello" + " " + name + "!";
+        return greetingName;
     }
 }
--- src/com/improve10x/tddpractise/namegreeting/NameGreetingTest.java
+++ src/com/improve10x/tddpractise/namegreeting/NameGreetingTest.java
@@ -12,6 +12,36 @@ public class NameGreetingTest {
     public void givenEmpty_returnEmpty() {
         Greeting greeting = new Greeting();
         String empty = greeting.greetingName("");
-        assertEquals("", empty);
+        assertEquals("Hello !", empty);
     }
+
+    @Test
+    public void givenGerald_returnHelloGeraldSymbol() {
+        Greeting greeting = new Greeting();
+        String name = greeting.greetingName("Gerald");
+        assertEquals("Hello Gerald!", name);
+    }
+
+    @Test
+    public void givenEd_returnHelloEdSymbol() {
+        Greeting greeting = new Greeting();
+        String name = greeting.greetingName("Ed");
+        assertEquals("Hello Ed!", name);
+    }
+    @Test
+    public void givenRoopa_returnHelloRoopaSymbol() {
+        Greeting greeting = new Greeting();
+        String name = greeting.greetingName("Roopa");
+        assertEquals("Hello Roopa!", name);
+    }
+
+    @Test
+    public void givenTiffany_returnHelloTiffanySymbol() {
+        Greeting greeting = new Greeting();
+        String name = greeting.greetingName("Tiffany");
+        assertEquals("Hello Tiffany!", name);
+    }
+
+
+
 }
