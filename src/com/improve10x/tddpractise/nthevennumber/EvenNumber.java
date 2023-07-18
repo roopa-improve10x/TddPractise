b8b75973ddca9ee790b0ad2daeb42ec30cd98019
roopa <roopa.peddinti@improve10x.com>
2023-07-18 12:27:46 +0530

    test cases added for nth even number classes and updated the document

--- src/com/improve10x/tddpractise/nthevennumber/EvenNumber.java
+++ src/com/improve10x/tddpractise/nthevennumber/EvenNumber.java
@@ -2,9 +2,16 @@ package com.improve10x.tddpractise.nthevennumber;
 
 public class EvenNumber {
 
-    public int isEven(int num) {
-        int nthEvenNumber;
-        nthEvenNumber = 2*num - 2;
+    public int findNthEvenNumber(int num) {
+        int nthEvenNumber = 0;
+        if(num == 0) {
+            return 1;
+        } else if(num < 0 ) {
+            return  -1;
+        } else if(num > 0) {
+            nthEvenNumber = 2 * num - 2;
+            return nthEvenNumber;
+        }
         return nthEvenNumber;
     }
 }
--- src/com/improve10x/tddpractise/nthevennumber/NthEvenTest.java
+++ src/com/improve10x/tddpractise/nthevennumber/NthEvenTest.java
@@ -9,37 +9,52 @@ public class NthEvenTest {
     public void nothing() {}
 
     @Test
-    public void giveZero_returnZerothNumber() {
+    public void givenNegative_returnNegativeNumber() {
         EvenNumber evenNumber = new EvenNumber();
-        int result = evenNumber.isEven(0);
-        assertEquals(-2, result);
+        int result = evenNumber.findNthEvenNumber(-10);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenNegativeValue_returnMinusOne() {
+        EvenNumber evenNumber = new EvenNumber();
+        int result = evenNumber.findNthEvenNumber(-3);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenZero_returnOne() {
+        EvenNumber evenNumber = new EvenNumber();
+        int result = evenNumber.findNthEvenNumber(0);
+        assertEquals(1, result);
     }
 
     @Test
     public void givenTwo_returnSecondEvenNumber() {
         EvenNumber evenNumber = new EvenNumber();
-        int result = evenNumber.isEven(2);
+        int result = evenNumber.findNthEvenNumber(2);
         assertEquals(2, result);
     }
 
     @Test
     public void givenTwenty_return20thEvenNumber() {
         EvenNumber evenNumber = new EvenNumber();
-        int result = evenNumber.isEven(20);
+        int result = evenNumber.findNthEvenNumber(20);
         assertEquals(38, result);
     }
 
     @Test
     public void given100_return100thEvenNumber() {
         EvenNumber evenNumber = new EvenNumber();
-        int result = evenNumber.isEven(100);
-        assertEquals(198, result);
+        int result = evenNumber.findNthEvenNumber(100);
+        assertEquals(98, result);
     }
 
     @Test
-    public void givenNegativeNumber_returnNegativeNumber() {
+    public void given2000_return2000thEvenNumber() {
         EvenNumber evenNumber = new EvenNumber();
-        int result = evenNumber.isEven(-5);
-        assertEquals(-12, result);
+        int result = evenNumber.findNthEvenNumber(2000);
+        assertEquals(3998, result);
     }
+
 }
