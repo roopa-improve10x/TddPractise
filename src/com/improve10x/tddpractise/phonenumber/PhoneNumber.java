d97aa9f890736ef5c29e1147e91de140a0962bb6
roopa <roopa.peddinti@improve10x.com>
2023-07-20 15:13:25 +0530

    Test cases modified for the Format phone number program

--- src/com/improve10x/tddpractise/phonenumber/PhoneNumber.java
+++ src/com/improve10x/tddpractise/phonenumber/PhoneNumber.java
@@ -3,8 +3,12 @@ package com.improve10x.tddpractise.phonenumber;
 public class PhoneNumber {
     public String getPhoneNumber(int[] number) {
         String formatPhoneNumber = "";
-        formatPhoneNumber = "(" + number[0] + number[1] + number[2] + ")" + " " + number[3] +
-                number[4] + number[5] + "-" + number[6] + number[7] + number[8] + number[9];
+        if(number.length < 10) {
+            return Integer.toString(-1);
+        } else if(number.length == 10) {
+            formatPhoneNumber = "(" + number[0] + number[1] + number[2] + ")" + " " + number[3] +
+                    number[4] + number[5] + "-" + number[6] + number[7] + number[8] + number[9];
+        }
         return formatPhoneNumber;
     }
 }
--- src/com/improve10x/tddpractise/phonenumber/PhoneNumberTest.java
+++ src/com/improve10x/tddpractise/phonenumber/PhoneNumberTest.java
@@ -1,39 +1,61 @@
 package com.improve10x.tddpractise.phonenumber;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class PhoneNumberTest {
 
+    private PhoneNumber phoneNumber;
+
+    @Before
+    public void setUp() {
+        phoneNumber = new PhoneNumber();
+    }
+
     @Test
     public void nothing() {}
 
     @Test
-    public void givenEmptyArray_returnString() {
-        PhoneNumber number = new PhoneNumber();
-        String formatPhoneNum = number.getPhoneNumber(new int[] {});
-        assertEquals("", formatPhoneNum);
+    public void givenEmptyArray_returnMinusOne() {
+        String formatPhoneNum = phoneNumber.getPhoneNumber(new int[] {});
+        assertEquals("-1", formatPhoneNum);
+    }
+
+    @Test
+    public void givenArrayOfOneElement_returnMinusOne() {
+        String formatPhoneNumber = phoneNumber.getPhoneNumber(new int[] {1});
+        assertEquals("-1", formatPhoneNumber);
+    }
+
+    @Test
+    public void givenArrayOfTwoElements_returnMinusOne() {
+        String formatPhoneNumber = phoneNumber.getPhoneNumber(new int[] {1, 2});
+        assertEquals("-1", formatPhoneNumber);
+    }
+
+    @Test
+    public void givenArrayOfFourElements_returnMinusOne() {
+        String formatPhoneNumber = phoneNumber.getPhoneNumber(new int[] {0, 1, 3, 2});
+        assertEquals("-1", formatPhoneNumber);
     }
 
     @Test
     public void givenArrayElements_returnString() {
-        PhoneNumber number = new PhoneNumber();
-        String formatPhoneNumber = number.getPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+        String formatPhoneNumber = phoneNumber.getPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
         assertEquals("(123) 456-7890", formatPhoneNumber);
     }
 
     @Test
     public void givenTenElementsOfArray_returnString() {
-        PhoneNumber number = new PhoneNumber();
-        String formatPhoneNumber = number.getPhoneNumber(new int[] {0, 4, 4, 1, 3, 8, 9, 8, 7, 6});
+        String formatPhoneNumber = phoneNumber.getPhoneNumber(new int[] {0, 4, 4, 1, 3, 8, 9, 8, 7, 6});
         assertEquals("(044) 138-9876", formatPhoneNumber);
     }
 
     @Test
     public void givenArrayWithTenValues_returnString() {
-        PhoneNumber number = new PhoneNumber();
-        String formatPhoneNumber = number.getPhoneNumber(new int[] {1, 5, 7, 4, 3, 2, 7, 0, 8, 9});
+        String formatPhoneNumber = phoneNumber.getPhoneNumber(new int[] {1, 5, 7, 4, 3, 2, 7, 0, 8, 9});
         assertEquals("(157) 432-7089", formatPhoneNumber);
     }
 }
