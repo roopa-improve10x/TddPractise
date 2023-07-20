855175a79af40eb072575923f469320e38cc651d
roopa <roopa.peddinti@improve10x.com>
2023-07-20 16:58:24 +0530

    Test cases modified for the Two makes ten program

--- src/com/improve10x/tddpractise/twomakesten/MakesTen.java
+++ src/com/improve10x/tddpractise/twomakesten/MakesTen.java
@@ -4,10 +4,12 @@ public class MakesTen {
     public boolean makesTen(int a, int b) {
         boolean result = false;
         int sum = a + b;
-        if(sum == 10) {
-            result = false;
+        if(sum < 10) {
+            return false;
         } else if(sum == a || sum == b) {
             result = true;
+        } else if(sum == 10) {
+            return  true;
         }
         return result;
     }
--- src/com/improve10x/tddpractise/twomakesten/MakesTenTest.java
+++ src/com/improve10x/tddpractise/twomakesten/MakesTenTest.java
@@ -17,26 +17,68 @@ public class MakesTenTest {
     }
 
     @Test
-    public void givenSum_returnEqualOrNotToTen() {
-        boolean isEqual = ten.makesTen(10, 9);
+    public void givenZeroZero_returnFalse() {
+        boolean isEqual = ten.makesTen(0, 0);
         assertEquals(false, isEqual);
     }
 
     @Test
-    public void givenTwoValues_returnTrueOrFalse() {
-        boolean isEqualsTen = ten.makesTen(1, 9);
+    public void givenZeroOne_returnFalse() {
+        boolean isEqualsTen = ten.makesTen(0, 1);
         assertEquals(false, isEqualsTen);
     }
 
     @Test
-    public void givenTwoInputs_returnResult() {
-        boolean result = ten.makesTen(9,9);
-        assertEquals(false, result);
+    public void givenOneTwo_returnFalse() {
+        boolean isEqualsTen = ten.makesTen(1, 2);
+        assertEquals(false, isEqualsTen);
     }
 
     @Test
-    public void givenTwo_returnResult() {
-        boolean result = ten.makesTen(2, 8);
-        assertEquals(false, result);
+    public void givenTwoThree_returnFalse() {
+        boolean isEqualsTen = ten.makesTen(2, 3);
+        assertEquals(false, isEqualsTen);
+    }
+
+    @Test
+    public void givenThreeFour_returnFalse() {
+        boolean isEqualsTen = ten.makesTen(3, 4);
+        assertEquals(false, isEqualsTen);
+    }
+
+    @Test
+    public void givenFourFive_returnFalse() {
+        boolean isEqualsTen = ten.makesTen(4, 5);
+        assertEquals(false, isEqualsTen);
+    }
+
+    @Test
+    public void givenFiveFive_returnTrue() {
+        boolean isEqualsTen = ten.makesTen(5, 5);
+        assertEquals(true, isEqualsTen);
+    }
+
+    @Test
+    public void givenSixFour_returnTrue() {
+        boolean isEqualsTen = ten.makesTen(6, 4);
+        assertEquals(true, isEqualsTen);
+    }
+
+    @Test
+    public void givenSevenThree_returnTrue() {
+        boolean isEqualsTen = ten.makesTen(7, 3);
+        assertEquals(true, isEqualsTen);
+    }
+
+    @Test
+    public void givenEightTwo_returnTrue() {
+        boolean isEqualsTen = ten.makesTen(8, 2);
+        assertEquals(true, isEqualsTen);
+    }
+
+    @Test
+    public void givenNineOne_returnTrue() {
+        boolean isEqualsTen = ten.makesTen(9, 1);
+        assertEquals(true, isEqualsTen);
     }
 }
